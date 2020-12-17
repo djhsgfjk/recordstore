bdf1f4fe4e6e3380ee662e5639661ec87e4e8eda
djhsgfjk <butyakova01@mail.ru>
2020-12-17 10:51:02 +0300

    'v4'

---
+++ RecordStore/RecordStore/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecordStore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+        }
+    }
+}
